41fd44e1dfdee2447604cb5513b4c19161b5b826
Sakib Mahmud <52151316+Sigmakib2@users.noreply.github.com>
2024-05-16 07:20:44 +0600

    cf debug 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://louisescher.github.io',
-  base: '/starlight-ion-theme',
+  base: '/',
   integrations: [starlight({
     title: 'Ion Theme',
     logo: {
